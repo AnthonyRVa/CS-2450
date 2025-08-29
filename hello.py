9f9cb00f57b1952231d975b76edccc9230f5b52e
AnthonyRVa <anthonyrubiovasquez@gmail.com>
2025-08-29 10:38:35 -0600

    updated hello age code

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ print("hello world")
 
 def start():
     name = input("What's your name")
-    age = range(15,31)
+    age = random.randrange(15,30)
     ans = None
     while ans != "Y" or ans != "N":
         ans = input("Is this your age " + str(age) + "? (Y/N)")
@@ -12,3 +12,5 @@ def start():
         print("rats")
     else:
         print(name + " is " + str(age) + " years old.")
+
+start()
