821c35a08fc0b3382e4359438924ced93e26f05b
AnthonyRVa <anthonyrubiovasquez@gmail.com>
2025-08-29 10:04:12 -0600

    guess age code

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
+import random
+
 print("hello world")
+
+def start():
+    name = input("What's your name")
+    age = range(15,31)
+    ans = None
+    while ans != "Y" or ans != "N":
+        ans = input("Is this your age " + str(age) + "? (Y/N)")
+    if ans == "N":
+        print("rats")
+    else:
+        print(name + " is " + str(age) + " years old.")
